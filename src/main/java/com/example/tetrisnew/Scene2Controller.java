51e9420814cd5239bb4c4397c720941dc8ea7051
FahadToqir <cf19fahad.toqir@iesjoandaustria.org>
2022-10-24 16:10:26 +0200

    updated

--- src/main/java/com/example/tetrisnew/Scene2Controller.java
+++ src/main/java/com/example/tetrisnew/Scene2Controller.java
@@ -13,16 +13,26 @@ import java.util.ResourceBundle;
 public class Scene2Controller implements Initializable {
 
     @FXML
-    private Rectangle newRectangle;
+    private Rectangle newRectangle2;
+
+    @FXML
+    private Rectangle newRectangle3;
 
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1){
         TranslateTransition translate = new TranslateTransition();
-        translate.setNode(newRectangle);
+        TranslateTransition translate2 = new TranslateTransition();
+        translate.setNode(newRectangle2);
         translate.setDuration(Duration.seconds(12));
         translate.setCycleCount(1);
-        translate.setByY(470);
+        translate.setByY(510);
         translate.play();
+
+        translate2.setNode(newRectangle3);
+        translate2.setDuration(Duration.seconds(13));
+        translate2.setCycleCount(1);
+        translate2.setByY(490);
+        translate2.play();
     }
 }
