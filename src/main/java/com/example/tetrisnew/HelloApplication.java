1ffae0c38866af474039f1a60549cfd3e6ff8999
FahadToqir <cf19fahad.toqir@iesjoandaustria.org>
2022-10-26 15:48:21 +0200

    updated

--- src/main/java/com/example/tetrisnew/HelloApplication.java
+++ src/main/java/com/example/tetrisnew/HelloApplication.java
@@ -1,29 +1,18 @@
 package com.example.tetrisnew;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.text.Normalizer;
 
 public class HelloApplication extends Application {
-    public static final int MOVE = 25;
-    public static final int SIZE = 25;
-    public static int XMAX = SIZE * 12;
-    public static int YMAX = SIZE * 24;
-    public static int [][] MESH = new int [XMAX/SIZE][YMAX/SIZE];
-    private static Pane groupe = new Pane();
-    private static Normalizer.Form object;
-    private static Scene scene = new Scene(groupe, XMAX + 150, YMAX);
-    public static int score = 0;
-    public static boolean game = true;
-    private static Form nextObj = MovementController.makeRect();
-    private static int linesNo = 0;
-
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("scene2.fxml"));
@@ -39,7 +28,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
 
-
+        scene.setOnKeyPressed(Scene2Controller::keyPressed);
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/tetrisnew/Scene2Controller.java
+++ src/main/java/com/example/tetrisnew/Scene2Controller.java
@@ -3,6 +3,8 @@ package com.example.tetrisnew;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
@@ -13,7 +15,7 @@ import java.util.ResourceBundle;
 public class Scene2Controller implements Initializable {
 
     @FXML
-    private Rectangle newRectangle;
+    private static Rectangle newRectangle;
 
     @FXML
     private Rectangle newRectangle1;
@@ -25,7 +27,7 @@ public class Scene2Controller implements Initializable {
 
 
     @Override
-    public void initialize(URL arg0, ResourceBundle arg1){
+    public void initialize(URL arg0, ResourceBundle arg1) {
         TranslateTransition translate = new TranslateTransition();
         TranslateTransition translate2 = new TranslateTransition();
         TranslateTransition translate3 = new TranslateTransition();
@@ -34,25 +36,34 @@ public class Scene2Controller implements Initializable {
         translate3.setNode(newRectangle);
         translate3.setDuration(Duration.seconds(12));
         translate3.setCycleCount(1);
-        translate3.setByY(473);
+        translate3.setByY(475);
         translate3.play();
 
         translate4.setNode(newRectangle1);
         translate4.setDuration(Duration.seconds(12));
         translate4.setCycleCount(1);
-        translate4.setByY(475);
+        translate4.setByY(477);
         translate4.play();
 
         translate.setNode(newRectangle2);
         translate.setDuration(Duration.seconds(12));
         translate.setCycleCount(1);
-        translate.setByY(478);
+        translate.setByY(479);
         translate.play();
+
 
         translate2.setNode(newRectangle3);
         translate2.setDuration(Duration.seconds(12));
         translate2.setCycleCount(1);
         translate2.setByY(480);
         translate2.play();
+    }
+
+    public static void keyPressed(KeyEvent evt) {
+        KeyCode key = evt.getCode();  // keyboard code for the pressed key
+        System.out.println("Key Pressed: " + key);  // for testing
+        if (key == KeyCode.LEFT){  // left arrow key
+            newRectangle.setLayoutX(newRectangle.getLayoutX() + 2);
+        }
     }
 }
